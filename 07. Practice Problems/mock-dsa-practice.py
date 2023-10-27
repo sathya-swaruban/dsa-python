bcc4415755476a94b72ba7de89b1e1e502cc944a
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-10-27 23:54:38 +0530

    Create mock-dsa-practice.py

---
+++ 07. Practice Problems/mock-dsa-practice.py
@@ -0,0 +1,216 @@
+# lex_auth_012761717559443456689
+"""
+TODO: Problem Statement goes here
+"""
+
+
+# Linked List Implementation
+class Node:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
+
+    def get_data(self):
+        return self.__data
+
+    def set_data(self, data):
+        self.__data = data
+
+    def get_next(self):
+        return self.__next
+
+    def set_next(self, next_node):
+        self.__next = next_node
+
+
+class LinkedList:
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+
+    def get_head(self):
+        return self.__head
+
+    def get_tail(self):
+        return self.__tail
+
+    def set_head(self, new_node):
+        self.__head = new_node
+
+    def set_tail(self, new_node):
+        self.__tail = new_node
+
+    def add(self, data):
+        new_node = Node(data)
+        if self.__head is None:
+            self.__head = self.__tail = new_node
+        else:
+            self.__tail.set_next(new_node)
+            self.__tail = new_node
+
+    def insert(self, data, data_before):
+        new_node = Node(data)
+        if data_before is None:
+            new_node.set_next(self.__head)
+            self.__head = new_node
+            if new_node.get_next() is None:
+                self.__tail = new_node
+
+        else:
+            node_before = self.find_node(data_before)
+            if node_before is not None:
+                new_node.set_next(node_before.get_next())
+                node_before.set_next(new_node)
+                if new_node.get_next() is None:
+                    self.__tail = new_node
+            else:
+                print(data_before, "is not present in the Linked list")
+
+    def display(self):
+        temp = self.__head
+        while temp is not None:
+            print(temp.get_data(), '->', end=' ')
+            temp = temp.get_next()
+        print()
+
+    def find_node(self, data):
+        temp = self.__head
+        while temp is not None:
+            if temp.get_data() == data:
+                return temp
+            temp = temp.get_next()
+        return None
+
+    def delete(self, data):
+        node = self.find_node(data)
+        if node is not None:
+            if node == self.__head:
+                if self.__head == self.__tail:
+                    self.__tail = None
+                self.__head = node.get_next()
+            else:
+                temp = self.__head
+                while temp is not None:
+                    if temp.get_next() == node:
+                        temp.set_next(node.get_next())
+                        if node == self.__tail:
+                            self.__tail = temp
+                        node.set_next(None)
+                        break
+                    temp = temp.get_next()
+        else:
+            print(data, "is not present in Linked list")
+
+
+# Queue Implementation
+class Queue:
+    def __init__(self, max_size):
+        self.__max_size = max_size
+        self.__elements = [None] * self.__max_size
+        self.__rear = -1
+        self.__front = 0
+
+    def is_full(self):
+        return self.__rear == self.__max_size - 1
+
+    def is_empty(self):
+        return self.__front > self.__rear
+
+    def enqueue(self, data):
+        if self.is_full():
+            print("Queue is full!")
+        else:
+            self.__rear += 1
+            self.__elements[self.__rear] = data
+
+    def dequeue(self):
+        if self.is_empty():
+            print("Queue is empty!")
+        else:
+            data = self.__elements[self.__front]
+            for i in range(self.__rear):
+                self.__elements[i] = self.__elements[i + 1]
+            self.__rear -= 1
+            return data
+
+    def display(self):
+        for index in range(self.__front, self.__rear + 1):
+            print(self.__elements[index], end='<-')
+        print()
+
+    def get_max_size(self):
+        return self.__max_size
+
+
+# Stack Implementation
+class Stack:
+    def __init__(self, max_size):
+        self.__max_size = max_size
+        self.__elements = [None] * self.__max_size
+        self.__top = -1
+
+    def is_full(self):
+        if self.__top == self.__max_size - 1:
+            return True
+        return False
+
+    def is_empty(self):
+        if self.__top == -1:
+            return True
+        return False
+
+    def push(self, data):
+        if self.is_full():
+            print("The stack is full!!")
+        else:
+            self.__top += 1
+            self.__elements[self.__top] = data
+
+    def pop(self):
+        if self.is_empty():
+            print("The stack is empty!!")
+        else:
+            data = self.__elements[self.__top]
+            self.__top -= 1
+            return data
+
+    def display(self):
+        if self.is_empty():
+            print("The stack is empty")
+        else:
+            index = self.__top
+            while index >= 0:
+                print(self.__elements[index])
+                index -= 1
+
+    def get_max_size(self):
+        return self.__max_size
+
+
+def queue_ordering(input_queue, input_stack):
+    output_queue = Queue(input_queue.get_max_size())
+    while not input_stack.is_empty():
+        element = input_stack.pop()
+        if element == 1:
+            input_queue.enqueue(input_queue.dequeue())
+        elif element == 2:
+            # TODO: Complete the code.
+            pass
+    return output_queue
+
+
+if __name__ == '__main__':
+    stack = Stack(5)
+    stack.push(1)
+    stack.push(1)
+    stack.push(1)
+    stack.push(1)
+    stack.push(1)
+    queue = Queue(5)
+    queue.enqueue('C')
+    queue.enqueue('A')
+    queue.enqueue('B')
+    queue.enqueue('D')
+    queue.enqueue('A')
+    result = queue_ordering(queue, stack)
+    result.display()
