7ff9666d2775ef48b040f5f2ddcfa6c7c31db52f
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-10-29 19:41:22 +0530

    Create practice problem 14

---
+++ 07. Practice Problems/Level 3/problem-14.py
@@ -0,0 +1,12 @@
+# lex_auth_012751758174461952110
+"""
+TODO: Problem Statement goes here
+"""
+
+
+def find_minimum_time(no_of_assignee, time_per_unit_job, job_time_list):
+    pass
+
+
+if __name__ == '__main__':
+    print(find_minimum_time(4, 5, [11, 4, 5, 18, 20, 13, 7, 10, 3]))
